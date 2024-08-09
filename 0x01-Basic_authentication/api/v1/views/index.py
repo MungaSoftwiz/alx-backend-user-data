7751ccf1b4621e41d9e37650af2f5704af59d8ca
MungaSoftwiz <mwachilumobm@gmail.com>
2024-08-09 20:00:07 +0300

    Add an unauthorized route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unathorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - the status of an unauthorized route
+    """
+    abort(401)
