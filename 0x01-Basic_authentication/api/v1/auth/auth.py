a5ffa7b9033ccfd6445c434aab854177a10a73f0
MungaSoftwiz <mwachilumobm@gmail.com>
2024-08-27 18:39:53 +0300

    Add authentication template

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Authentication class """
+from flask import request
+from typing import (List, TypeVar)
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method checks if authentication is required for a given path """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Method returns the authorization header from the request """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Returns the current user """
+        return None
